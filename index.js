987f0e0eed8e9b79fafc41de896c1c3381d327c7
Carlos Pires <extraterra1@gmail.com>
2023-08-17 14:45:51 +0100

    feat: add support for multiple searches

--- index.js
+++ index.js
@@ -1,58 +1,76 @@
+import "dotenv/config";
+
 import axios from "axios";
 import cheerio from "cheerio";
 import { Telegraf } from "telegraf";
-const app = new Telegraf("6156365798:AAGSth_AVl833DGcyJmABzg3nvwXTA6XyPA");
-const chatId = "741910784";
+import { promisify } from "util";
+import fs from "fs";
+
+const app = new Telegraf(process.env.BOT_KEY);
+const chatId = process.env.CHAT_ID;
+
+//TIME BETWEEN SEARCHES
+const interval = 600000;
+
 const SPECIAL_CHARS = ["\\", "_", "*", "[", "]", "(", ")", "~", "`", ">", "<", "&", "#", "+", "-", "=", "|", "{", "}", ".", "!"];
 const escapeMarkdown = (text) => {
   SPECIAL_CHARS.forEach((char) => (text = text.replaceAll(char, `\\${char}`)));
   return text;
 };
+
 const items = [];
+const readFileAsync = promisify(fs.readFile);
+const getUrls = async () => {
+  const data = await readFileAsync("urls.txt");
+  return data.toString().split("\n");
+};
+const urls = await getUrls();
 
-setInterval(() => {
-  (async () => {
-    const response = await axios.get(
-      "https://www.olx.pt/ads/q-RYZEN/?search%5Border%5D=created_at:desc&search%5Bfilter_float_price:from%5D=10&search%5Bfilter_float_price:to%5D=200&reason=observed_search"
-    );
-    const html = response.data;
-    console.log(items.length);
+for (const url of urls) {
+  setInterval(() => {
+    (async () => {
+      //get search term
+      const term = decodeURIComponent(url.match(/q-(.*?)(?=\/)/)[1]).replace("-", " ");
+      console.log(`Fetching ${term}`);
+      const response = await axios.get(url);
+      const html = response.data;
 
-    // Use Cheerio to parse the HTML
-    const $ = cheerio.load(html);
-    const list = $(".css-1sw7q4x").splice(0, 5);
+      // Use Cheerio to parse the HTML
+      const $ = cheerio.load(html);
+      const list = $(".css-1sw7q4x").splice(0, 5);
 
-    for (const e of list) {
-      const title = $(e).find(".css-16v5mdi").text();
-      const priceStr = $(e).find(".css-10b0gli").text();
-      const price = priceStr
-        .split("€")[0]
-        .replace(/\..*}(.*)/, "$1")
-        .replace(/\D/g, "");
-      const url = "https://www.olx.pt" + $(e).find(".css-rc5s2u").attr("href");
-      const img = $(e).find(".css-gl6djm img").attr("src");
-      const id = url
-        .substring(url.lastIndexOf("/") + 1, url.lastIndexOf("."))
-        .split("-")
-        .slice(-1)
-        .toString();
-      const itemToPush = {
-        title: title,
-        price: price,
-        url: url,
-        img: img,
-        id: id,
-      };
-      const index = items.findIndex((e) => e.id == id);
-      if (index === -1) {
-        items.push(itemToPush);
-        console.log(`Inserted ${title} ${price}€`);
-        if (items.length > 5) {
-          const caption = `🔴 *NEW* listing\n\n🗨️${escapeMarkdown(title)}\n💰*${price}€*\n`;
-          const escapedCaption = caption + `🔗[OLX](${url})`;
-          app.telegram.sendPhoto(chatId, { url: img }, { caption: escapedCaption, parse_mode: "MarkdownV2" });
+      for (const e of list) {
+        const title = $(e).find(".css-16v5mdi").text();
+        const priceStr = $(e).find(".css-10b0gli").text();
+        const price = priceStr
+          .split("€")[0]
+          .replace(/\..*}(.*)/, "$1")
+          .replace(/\D/g, "");
+        const url = "https://www.olx.pt" + $(e).find(".css-rc5s2u").attr("href");
+        const img = $(e).find(".css-gl6djm img").attr("src");
+        const id = url
+          .substring(url.lastIndexOf("/") + 1, url.lastIndexOf("."))
+          .split("-")
+          .slice(-1)
+          .toString();
+        const itemToPush = {
+          title: title,
+          price: price,
+          url: url,
+          img: img,
+          id: id,
+        };
+        const index = items.findIndex((e) => e.id == id);
+        if (index === -1) {
+          items.push(itemToPush);
+          console.log(`Inserted ${title} ${price}€`);
+          if (items.length > urls.length) {
+            const caption = `🔴 *NEW* listing\n\n🗨️${escapeMarkdown(title)}\n💰*${price}€*\n`;
+            const escapedCaption = caption + `🔗[OLX](${url})`;
+            app.telegram.sendPhoto(chatId, { url: img }, { caption: escapedCaption, parse_mode: "MarkdownV2" });
+          }
         }
       }
-    }
-  })();
-}, 600000);
+    })();
+  }, interval);
+}
---
+++ readme.md
@@ -0,0 +1,68 @@
+# OLX Monitor
+
+Hi! this is a simple bot to monitor newly listed products on the website OLX (Portugal's eBay). Built in Node.JS using Axios and Cheerio. The bot crawls through every URL you feed it and then sends you a Telegram message if it finds a new product. It uses Telegraf.js to handle the Telegram Bot API.
+
+Olá! Este é um bot simples para monitorizar os novos produtos listados no site OLX (eBay de Portugal). Construído em Node.JS usando Axios e Cheerio. O bot rastreia todos os URLs que lhe são fornecidos e envia uma mensagem de Telegram se encontrar um novo produto. Usa Telegraf.js para lidar com a API do Telegram.
+
+Hola! este es un simple bot para monitorear los productos recién listados en el sitio web OLX (eBay de Portugal). Construido en Node.JS usando Axios y Cheerio. El bot rastrea a través de cada URL que le das y luego te envía un mensaje de Telegram si encuentra un nuevo producto. Utiliza Telegraf.js para manejar la API de Telegram.
+
+## Getting Started
+
+### Prerequisites
+
+You obviously need [Node.JS](https://nodejs.org/en/download/current) to get this to work
+Using [Bash](https://git-scm.com/download/win) is reccomended
+
+### Installation
+
+_Below is an example of how you can instruct your audience on installing and setting up your app. This template doesn't rely on any external dependencies or services._
+
+1. Clone the repo
+   ```sh
+   git clone https://github.com/Extraterra1/OLX-Monitor.git
+   ```
+2. Install NPM packages
+   ```sh
+   npm install
+   ```
+3. Make your .env file and add your Telegram Bot Key and the Chat ID where it's supposed to send the alerts.
+   ```js
+   BOT_KEY = "ENTER YOUR BOT KEY";
+   CHAT_ID = "ENTER YOUR CHAT ID";
+   ```
+4. Create a new txt file called **"urls.txt"**. This is where you'll paste the URLs for every term you want to monitor.
+   ```js
+   https://www.olx.pt/tecnologia-e-informatica/computadores-informatica/componentes/q-fonte-alimenta%C3%A7%C3%A3o/?search%5Border%5D=created_at:desc&reason=observed_search
+   https://www.olx.pt/tecnologia-e-informatica/computadores-informatica/componentes/q-rtx-3080/?search%5Border%5D=created_at:desc&search%5Bfilter_float_price:from%5D=300&search%5Bfilter_float_price:to%5D=450&reason=observed_search
+   https://www.olx.pt/ads/q-RYZEN/?search%5Border%5D=created_at:desc&search%5Bfilter_float_price:from%5D=10&search%5Bfilter_float_price:to%5D=200&reason=observed_search
+   ```
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+## Running
+
+Time to get this started. Just open your console and type the next command
+
+```js
+   node index.js
+```
+
+After that you'll start to see your console light up.
+
+The bot updates every 10 minutes by defaul, you can change this in index.js by altering the "interval" variable
+
+```js
+const interval = 600000;
+// change 60000 for your desired interval, keep in mind this is in miliseconds
+// 1 minute = 60 seconds = 60000 ms
+```
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## [](https://www.makeareadme.com/#license)License
+
+[MIT](https://choosealicense.com/licenses/mit/)
